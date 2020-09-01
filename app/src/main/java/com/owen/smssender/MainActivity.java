e1adf3ededee3eee0b27a826256e17f7a414fa24
TimzOwen <ktimothy@kabarak.ac.ke>
2020-09-01 18:18:08 +0300

    soln to intent parsing and Uri

--- app/src/main/java/com/owen/smssender/MainActivity.java
+++ app/src/main/java/com/owen/smssender/MainActivity.java
@@ -2,7 +2,14 @@ package com.owen.smssender;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.net.URI;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //step 2. after UI XML design, we now create a function to send the sms
+    public void sendMessage(View view)
+    {
+        //Step 3. get the sms and send over an intent
+        EditText mMessageEt = findViewById(R.id.etMessage);
+        String message = mMessageEt.getText().toString();
+
+        //step 5. declare the destination using URI
+        Uri destination = Uri.parse("smsto:5556");
+
+        //Step 4.
+        //create an Implicit intent and specify the destination
+        Intent smsIntent = new Intent(Intent.ACTION_SEND,destination);
+        smsIntent.putExtra("sms_field",message);
+        startActivity(smsIntent);
+
+        //final step:
+        //create two emulator and test the sms .CONGRATULATIONS !!!!!
     }
 }
